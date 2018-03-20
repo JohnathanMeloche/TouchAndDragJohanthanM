216bc316172d436e5b18a76a98a18adb9e7f29cb
Johnathan Meloche <johnathan.meloche@ocsbstudent.ca>
2018-03-20 10:46:29 -0400

    sounds

--- main.lua
+++ main.lua
@@ -7,7 +7,25 @@
 display.setStatusBar(display.HiddenStatusBar)
 
 
-local Ting = audio.loadSound("sounds/ting.mp3")
+local Ting = audio.loadSound("Sounds/ding.mp3")
+local playTing
+local optionsTing = 
+{
+	channel = 1,
+	duration = 1000
+}
+audio.setVolume ( 1, optionsTing)
+
+local backgroundMusic = audio.loadSound ("Sounds/Party.mp3")
+local backgroundMusicChannel
+local optionsbackgroundMusic =
+{
+	channel = 2
+}
+audio.setVolume ( 0.01, optionsbackgroundMusic)
+--	backgroundMusicChannel = audio.play (backgroundMusic)
+
+
 
 --background image
 local backgroundImage = display.newImageRect("Images/background.jpg", 1920, 1920)
@@ -38,6 +56,7 @@ local function shirtListener(touch)
 	if (touch.phase == "began") then
 		if (alreadyTouchedpants == false) then
 			alreadyTouchedshirt = true	
+			playTing = audio.play(Ting)
 
 		end
 
@@ -77,6 +96,7 @@ local function pantsListener(touch)
 	if (touch.phase == "began") then
 		if (alreadyTouchedshirt == false) then
 			alreadyTouchedpants = true	
+			playTing = audio.play(Ting)
 
 		end
 
@@ -101,7 +121,3 @@ pants:addEventListener("touch", pantsListener)
 
 local commandtext = display.newText ("Dress Link up!", display.contentWidth/2, 200, "Images/vinet.ttf", 140)
 	commandtext:setFillColor (1, 0.2, 0)
-
-local backgroundMusic = audio.loadSound ("Sounds/Party.mp3")
-
-audio.play (backgroundMusic)
